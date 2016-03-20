d29cc1a867db11ac3b450720349e5ab900a62dc9
Stephen Chen <stephen.chen@uberflip.com>
2016-03-20 11:58:12 -0400

    Fixing port for heroku

--- api.py
+++ api.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, url_for
 
 app = Flask(__name__)
@@ -13,4 +14,6 @@ def rofl():
     return ':slightly_smiling_face: :upside_down_face: :slightly_smiling_face: :upside_down_face: :slightly_smiling_face: :upside_down_face: :slightly_smiling_face:'
 
 if __name__ == '__main__':
-    app.run()
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
